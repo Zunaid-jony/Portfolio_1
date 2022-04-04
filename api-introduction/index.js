978db0e6b98708366eeaac43403c0f1a8e95d94c
Zunaid-jony <jony35666666@gmail.com>
2022-04-04 15:19:38 +0600

    first commit

---
+++ api-introduction/index.js
@@ -0,0 +1,27 @@
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(response => response.json())
+//   .then(json => console.log(json))
+function loadData(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => displayUsers(data))
+}
+function loadPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+function displayUsers(data){
+   const ul = document.getElementById('users');
+   for(const user of data){
+      const li = document.createElement('li');
+      li.innerText = `name: ${user.name}, Email: ${user.email}`;
+      ul.appendChild(li) 
+   }
+
+}
